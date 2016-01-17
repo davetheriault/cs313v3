879cbb9ec13e173c7c8a824d92277633e775ad8f
davault <davetheriault@gmail.com>
2016-01-16 23:39:10 -0700

    create homepage

---
+++ cs313v3/includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <link type="text/css" rel="stylesheet" href="css/style.css" />
+        <link type="text/css" rel="stylesheet" href="css/w3.css" />
+        <link type="text/css" rel="stylesheet" href="css/font-awesome.css" />
+        <meta charset="UTF-8">
+        <title><?php echo $title; ?></title>
+    </head>
+    <body>
+        <div id="container">
+            <header class="w3-container w3-blue">
+                <nav class="w3-topnav w3-xxlarge">
+                        <a href="index.php"><i class="fa fa-home w3-xxlarge"></i>David Theriault</a>
+                        <a href="index.php">Link</a>
+                        <a href="index.php">Link</a>
+                </nav>
+            </header>
---
+++ cs313v3/index.php
@@ -0,0 +1,56 @@
+
+<?php include 'includes/header.php'; ?>
+
+<main class="w3-container">
+    <div class="w3-third w3-margin-top">
+        <div class="w3-container w3-card-4 w3-white w3-padding-0 w3-margin">
+            <h3 class="w3-red w3-padding w3-margin-0">Team Readiness Assignments</h3>
+            <ul class="w3-ul w3-large">
+                <li><a href="#">Week 3</a></li>
+                <li><a href="#">Week 4</a></li>
+                <li><a href="#">Week 5</a></li>
+                <li><a href="#">Week 6</a></li>
+                <li><a href="#">Week 7</a></li>
+                <li><a href="#">Week 8</a></li>
+                <li><a href="#">Week 9</a></li>
+                <li><a href="#">Week 10</a></li>
+                <li><a href="#">Week 11</a></li>
+                <li><a href="#">Week 12</a></li>
+                <li><a href="#">Week 13</a></li>
+                <li><a href="#">Week 14</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="w3-third w3-margin-top">
+        <div class="w3-container w3-card-4 w3-white w3-padding-0 w3-margin">
+            <h3 class="w3-green w3-padding w3-margin-0">PHP / Java Projects</h3>
+            <ul class="w3-ul w3-large">
+                <li><a href="#">Week 3</a></li>
+                <li><a href="#">Week 4</a></li>
+                <li><a href="#">Week 5</a></li>
+                <li><a href="#">Week 6</a></li>
+                <li><a href="#">Week 7</a></li>
+                <li><a href="#">Week 8</a></li>
+                <li><a href="#">Week 9</a></li>
+                <li><a href="#">Week 10</a></li>
+                <li><a href="#">Week 11</a></li>
+                <li><a href="#">Week 12</a></li>
+                <li><a href="#">Week 13</a></li>
+                <li><a href="#">Week 14</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="w3-third w3-margin-top">
+        <div class="w3-container w3-card-4 w3-padding-0 w3-margin">
+            <h3 class="w3-green w3-padding w3-margin-0">About Me</h3>
+            <div class="w3-container w3-padding-0 w3-white" style="overflow: hidden;">
+                <div class="w3-image">
+                    <img class="w3-half" src="images/face.jpg" alt="Profile Picture" />
+                </div>
+                <blockquote class="w3-half w3-blockquote w3-large w3-white w3-padding" style="bottom: 0;">"Hi. My name is David, and this is my site.</blockquote>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php include 'includes/footer.php'; ?>
