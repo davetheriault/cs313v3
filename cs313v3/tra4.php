94d6d9bde3e86fad8d76d8e7a168249a1fa5de1c
davault <davetheriault@gmail.com>
2016-01-31 00:36:52 -0700

    d

--- cs313v3/tra4.php
+++ cs313v3/tra4.php
@@ -20,97 +20,106 @@
 +------------------------+<br/>
 4 rows in set (0.00 sec)<br/>
 <br/>
+<br/>
 MariaDB [conferenceDB]> describe note;<br/>
 +---------+----------+------+-----+---------+----------------+<br/>
 | Field * | Type *** | Null | Key | Default | Extra ******** |<br/>
 +---------+----------+------+-----+---------+----------------+<br/>
-| id **** | int(11) *| NO   | PRI | NULL ** | auto_increment |<br/>
-| talk_id | int(11) *| YES  | MUL | NULL ** | ************** |<br/>
-| user_id | int(11) *| YES  | MUL | NULL ** | ************** |<br/>
-| content | longtext | YES  | --- | NULL ** | ************** |<br/>
-| date ** | date *** | YES  | *** | NULL ** | ************** |<br/>
+| id **** | int(11) *| N ** | PRI | NULL ** | auto_increment |<br/>
+| talk_id | int(11) *| Y ** | MUL | NULL ** | ************** |<br/>
+| user_id | int(11) *| Y ** | MUL | NULL ** | ************** |<br/>
+| content | longtext | Y ** | *** | NULL ** | ************** |<br/>
+| date ** | date *** | Y ** | *** | NULL ** | ************** |<br/>
 +---------+----------+------+-----+---------+----------------+<br/>
 5 rows in set (0.01 sec)<br/>
 <br/>
+<br/>
 MariaDB [conferenceDB]> describe speaker;<br/>
-+------------+-------------+-------+-----+---------+----------------+<br/>
-| Field **** | Type ****** | Null *| Key | Default | Extra ******** |<br/>
-+------------+-------------+-------+-----+---------+----------------+<br/>
-| id ******* | int(11) *** | NO ** | PRI | NULL ** | auto_increment |<br/>
-| first_name | varchar(30) | YES * | *** | NULL ** | ************** |<br/>
-| last_name  | varchar(30) | YES * | *** | NULL ** | ************** |<br/>
-+------------+-------------+----- -+-----+---------+----------------+<br/>
++-----------+-------------+-------+-----+---------+----------------+<br/>
+| Field *** | Type ****** | Null *| Key | Default | Extra ******** |<br/>
++-----------+-------------+-------+-----+---------+----------------+<br/>
+| id ****** | int(11) *** | NO ** | PRI | NULL ** | auto_increment |<br/>
+| first_name| varchar(30) | YES * | *** | NULL ** | ************** |<br/>
+| last_name | varchar(30) | YES * | *** | NULL ** | ************** |<br/>
++-----------+-------------+-------+-----+---------+----------------+<br/>
 3 rows in set (0.01 sec)<br/>
+<br/>
 <br/>
 MariaDB [conferenceDB]> describe talk;<br/>
 +------------+--------------+------+-----+---------+----------------+<br/>
-| Field      | Type         | Null | Key | Default | Extra          |<br/>
+| Field **** | Type ******* | Null | Key | Default | Extra ******** |<br/>
 +------------+--------------+------+-----+---------+----------------+<br/>
-| id         | int(11)      | NO   | PRI | NULL    | auto_increment |<br/>
-| conf_date  | date         | YES  |     | NULL    |                |<br/>
-| speaker_id | int(11)      | YES  | MUL | NULL    |                |<br/>
-| title      | varchar(100) | YES  |     | NULL    |                |<br/>
-| content    | longtext     | YES  |     | NULL    |                |<br/>
+| id ******* | int(11) **** | N ** | PRI | NULL ** | auto_increment |<br/>
+| conf_date _| date ******* | Y ** | *** | NULL ** | ************** |<br/>
+| speaker_id | int(11) **** | Y ** | MUL | NULL ** | ************** |<br/>
+| title **** | varchar(100) | Y ** | *** | NULL ** | ************** |<br/>
+| content ** | longtext *** | Y ** | *** | NULL ** | ************** |<br/>
 +------------+--------------+------+-----+---------+----------------+<br/>
 5 rows in set (0.01 sec)<br/>
 <br/>
+<br/>
 MariaDB [conferenceDB]> describe user;<br/>
 +----------+-------------+------+-----+---------+----------------+<br/>
-| Field    | Type        | Null | Key | Default | Extra          |<br/>
+| Field ** | Type ****** | Null | Key | Default | Extra ******** |<br/>
 +----------+-------------+------+-----+---------+----------------+<br/>
-| id       | int(11)     | NO   | PRI | NULL    | auto_increment |<br/>
-| username | varchar(20) | YES  |     | NULL    |                |<br/>
+| id ***** | int(11) *** | N ** | PRI | NULL ** | auto_increment |<br/>
+| username | varchar(20) | Y ** | *** | NULL ** | ************** |<br/>
 +----------+-------------+------+-----+---------+----------------+<br/>
 2 rows in set (0.01 sec)<br/>
 <br/>
 <br/>
 MariaDB [conferenceDB]> show create table user;<br/>
-+-------+-------------------------------------------------------+<br/>
-| Table | Create Table                                          |<br/>
-+-------+-------------------------------------------------------+<br/>
-| user  | CREATE TABLE `user` (<br/>
-  `id` int(11) NOT NULL AUTO_INCREMENT,<br/>
-  `username` varchar(20) DEFAULT NULL,<br/>
-  PRIMARY KEY (`id`)<br/>
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 |<br/>
-+-------+-------------------------------------------------------+<br/>
++------+-------------------------------------------------------+<br/>
+| Table| Create Table **************************************** |<br/>
++------+-------------------------------------------------------+<br/>
+| user | CREATE TABLE `user` ( ******************************* |<br/>
+| **** | `id` int(11) NOT NULL AUTO_INCREMENT, *************** |<br/>
+| **** | `username` varchar(20) DEFAULT NULL, **************** |<br/>
+| **** | PRIMARY KEY (`id`) ********************************** |<br/>
+| **** | ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ************** |<br/>
++------+-------------------------------------------------------+<br/>
 1 row in set (0.00 sec)<br/>
+<br/>
 <br/>
 MariaDB [conferenceDB]> show CREATE TABLE speaker;<br/>
 +---------+-----------------------------------------------------+<br/>
-| Table   | Create Table                                        |<br/>                                                                                                                                                    |
+| Table * | Create Table ************************************** |<br/>                                                                                                                                                    |
 +---------+-----------------------------------------------------+<br/>
-| speaker | CREATE TABLE `speaker` (<br/>
-  `id` int(11) NOT NULL AUTO_INCREMENT,<br/>
-  `first_name` varchar(30) DEFAULT NULL,<br/>
-  `last_name` varchar(30) DEFAULT NULL,<br/>
-  PRIMARY KEY (`id`)<br/>
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 |<br/>
+| speaker | CREATE TABLE `speaker` ( ************************** |<br/>
+| ******* | `id` int(11) NOT NULL AUTO_INCREMENT, ************* |<br/>
+| ******* | `first_name` varchar(30) DEFAULT NULL, ************ |<br/>
+| ******* | `last_name` varchar(30) DEFAULT NULL, ************* |<br/>
+| ******* | PRIMARY KEY (`id`) ******************************** |<br/>
+| ******* | ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ************ |<br/>
 +---------+-----------------------------------------------------+<br/>
 1 row in set (0.00 sec)<br/>
+<br/>
 <br/>
 MariaDB [conferenceDB]> show create table talk;<br/>
 +-------+-------------------------------------------------------+<br/>
-| Table | Create Table                                          |<br/>                                                                                                                                                                                                                                                                                                                                          
+| Table | Create Table **************************************** |<br/>                                                                                                                                                                                                                                                                                                                                          
 +-------+-------------------------------------------------------+<br/>
-| talk  | CREATE TABLE `talk` (<br/>
-  `id` int(11) NOT NULL AUTO_INCREMENT,<br/>
-  `conf_date` date DEFAULT NULL,<br/>
-  `speaker_id` int(11) DEFAULT NULL,<br/>
-  `title` varchar(100) DEFAULT NULL,<br/>
-  `content` longtext,<br/>
-  PRIMARY KEY (`id`),<br/>
-  KEY `FK_talk_speaker_id` (`speaker_id`),<br/>
+| talk  | CREATE TABLE `talk` ( ******************************* |<br/>
+| ***** | `id` int(11) NOT NULL AUTO_INCREMENT, *************** |<br/>
+| ***** | `conf_date` date DEFAULT NULL, ********************** |<br/>
+| ***** | `speaker_id` int(11) DEFAULT NULL, ****************** |<br/>
+| ***** | `title` varchar(100) DEFAULT NULL, ****************** |<br/>
+| ***** | `content` longtext, ********************************* |<br/>
+| ***** | PRIMARY KEY (`id`), ********************************* |<br/>
+|_______| KEY `FK_talk_speaker_id` (`speaker_id`), ************ |<br/>
   CONSTRAINT `FK_talk_speaker_id` FOREIGN KEY (`speaker_id`) REFERENCES `speaker` (`id`)<br/>
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 |<br/>
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1 |<br/>
 +-------+-------------------------------------------------------+<br/>
 1 row in set (0.00 sec)<br/>
 <br/>
+<br/>
 MariaDB [conferenceDB]> show create table note;<br/>
-+-------+-------------------------------------------------------+<br/>
-| Table | Create Table                                          |<br/>
-+-------+-------------------------------------------------------+<br/>
-| note  | CREATE TABLE `note` (<br/>
++------+-------------------------------------------------------+<br/>
+| Table| Create Table                                          |<br/>
++------+-------------------------------------------------------+<br/>
+| note |<br/>
+|______|<br/>
+CREATE TABLE `note` (<br/>
   `id` int(11) NOT NULL AUTO_INCREMENT,<br/>
   `talk_id` int(11) DEFAULT NULL,<br/>
   `user_id` int(11) DEFAULT NULL,<br/>
