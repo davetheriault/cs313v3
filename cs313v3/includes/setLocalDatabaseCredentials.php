2bb65e48e3be92a99684748148fd131671818bd9
davault <davetheriault@gmail.com>
2016-02-01 17:45:38 -0700

    sjkjl

---
+++ cs313v3/includes/setLocalDatabaseCredentials.php
@@ -0,0 +1,7 @@
+<?php
+
+    $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+    $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+    $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+    $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+    $dbName = "dave";
