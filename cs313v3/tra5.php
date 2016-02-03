ba190c8ad60c4421520fbc6a7eae5f57f5b1a597
davault <davetheriault@gmail.com>
2016-02-02 19:38:15 -0700

    tra5 style

--- cs313v3/tra5.php
+++ cs313v3/tra5.php
@@ -6,20 +6,21 @@
 
 <main class="w3-container">
     <div class="w3-row">
-        <div class="w3-col s0 m0 l1" style="line-height: 0.1;">&nbsp;</div>
-        <div class="rel w3-col s12 m12 l10">
+        <div class="w3-col s0 m1 l2" style="line-height: 0.1;">&nbsp;</div>
+        <div class="rel w3-col s10 m12 l8">
 
             <div class="w3-container w3-card-4 w3-white w3-padding-0 w3-margin">
                 <h2 class="w3-red w3-padding w3-margin-0">Scriptures</h2>
 
                 <div class="w3-container w3-padding-bottom-8">
-                    <?php
-                    foreach ($db->query('SELECT book, chapter, verse, content FROM scripture') as $row) {
-                        echo '<div class="block"><strong>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</strong> - "'
-                        . $row['content'] . '"</div>';
-                    }
-                    ?>
-
+                    <ul class="w3-ul">
+                        <?php
+                        foreach ($db->query('SELECT book, chapter, verse, content FROM scripture') as $row) {
+                            echo '<li><strong>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</strong> - "'
+                            . $row['content'] . '"</li>';
+                        }
+                        ?>
+                    </ul>
 
                 </div>
             </div>
