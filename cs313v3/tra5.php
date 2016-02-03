dccc5a647d3f4cea5120893aec3ecb699d6cb91a
davault <davetheriault@gmail.com>
2016-02-02 21:41:13 -0700

    fdsf

--- cs313v3/tra5.php
+++ cs313v3/tra5.php
@@ -11,7 +11,7 @@
                 <h2 class="w3-red w3-padding w3-margin-0">Search</h2>
                 <div class="w3-container w3-padding-bottom-16" style="padding-top: 15px; padding-bottom: 15px;">
                     <p>Find entries by book...</p>
-                    <form id="ookbay" method="get" action="tra3.php">
+                    <form id="ookbay" method="get" action="tra5.php">
                         <label>Book:</label>
                         <input class="w3-input" type="text" name="book" />
                         <input class="w3-btn w3-blue" type="submit" value="Find" form="ookbay" />
@@ -27,7 +27,7 @@
                 <div class="w3-container w3-padding-bottom-16" style="padding-top: 15px; padding-bottom: 15px;">
                     <ul class="w3-ul">
                         <?php
-                        if (isset($_GET['book'])) {
+                        if ($_GET['book'] != '' && $_GET['book'] != NULL) {
                             foreach ($db->query('SELECT book, chapter, verse, content FROM scripture WHERE book = "' . $_GET['book'] . '"') as $row) {
                                 echo '<li><strong>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</strong> <br/>"'
                                 . $row['content'] . '"</li>';
