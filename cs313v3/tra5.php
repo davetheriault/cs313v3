0381048a1e3107bcaaa1d3ef65647d46a73a2d69
davault <davetheriault@gmail.com>
2016-02-02 21:09:22 -0700

    form 2

--- cs313v3/tra5.php
+++ cs313v3/tra5.php
@@ -7,8 +7,11 @@
 <main class="w3-container">
     <div class="w3-row">
         <div class="w3-col s4 m4 l4" style="line-height: 0.1;">
-            <div class="w3-container w3-card-4 w3-white">
-                <h2 class="w3-red w3-padding w3-margin-0">Scriptures</h2>
+            <div class="w3-container w3-card-4 w3-white w3-padding-0 w3-margin">
+                <h2 class="w3-red w3-padding w3-margin-0">Search</h2>
+                <div class="w3-container w3-padding-bottom-16" style="padding-top: 15px; padding-bottom: 15px;">
+                    Find entries by book...
+                </div>
             </div>
         </div>
         <div class="rel w3-col s8 m8 l8">
