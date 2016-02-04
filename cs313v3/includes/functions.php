e023981f40ad0b5aa51cbc93a78554ba2b90dbc1
davault <davetheriault@gmail.com>
2016-02-03 21:01:42 -0700

    goij

---
+++ cs313v3/includes/functions.php
@@ -0,0 +1,55 @@
+<?php
+
+function attempt_login($username, $password) {
+    $user = find_user_by_username($username);
+    if ($user) {
+        // found user, now check password
+        if (password_check($password, $user["hashed_password"])) {
+            // password matches
+            return $user;
+        } else {
+            // password does not match
+            return false;
+        }
+    } else {
+        // user not found
+        return false;
+    }
+}
+
+function password_check($password, $existing_hash) {
+    // existing hash contains format and salt at start
+    $hash = crypt($password, $existing_hash);
+    if ($hash === $existing_hash) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function find_user_by_username($username) {
+    global $db;
+
+    $safe_username = mysqli_real_escape_string($db, $username);
+
+
+    $query = "SELECT * ";
+    $query .= "FROM user ";
+    $query .= "WHERE username = '{$safe_username}' ";
+    $query .= "LIMIT 1";
+    $user_set = mysqli_query($db, $query);
+    confirm_query($user_set);
+    if ($user = mysqli_fetch_assoc($user_set)) {
+        return $user;
+    } else {
+        return null;
+    }
+}
+
+function confirm_query($result_set) {
+    if (!$result_set) {
+        die("Database query failed.");
+    }
+}
+
+?>
--- cs313v3/login.php
+++ cs313v3/login.php
@@ -1,27 +1,35 @@
+<?php 
+if (isset($_POST['login'])) {
+    $username = $_POST['user'];
+    $password = $_POST['pass'];
+    $found_user = attempt_login($username, $password);
+}
+?>
+
 <?php $title = 'Login'; ?>
 <?php include 'includes/header.php'; ?>
 
 <main class="w3-container">
     <div class="w3-row">
-        <div class="w3-col l2">&nbsp;</div>
-        <div class="w3-col l8">
+        <div class="w3-col s1 m2 l4">&nbsp;</div>
+        <div class="w3-col s10 m8 l4">
             <div class="w3-container w3-card-4 w3-white w3-padding-0 w3-margin">
                 <h3 class="w3-red w3-padding w3-margin-0">Log In</h3>
 
-                <form id="userLogin" action="" method="post" class="w3-container">
+                <form id="userLogin" action="login.php" method="post" class="w3-container">
                     <div class="w3-half w3-container">
                         <p>
                             <label>Username:</label>
-                            <input class="w3-input" type="text" name="user"/>
+                            <input class="w3-input" type="text" name="user" required="required"/>
                         </p>
                         <p>
                             <label>Password</label>
-                            <input class="w3-input" type="password" name="pass"/>
+                            <input class="w3-input" type="password" name="pass" required="required"/>
                         </p>
                         
                     </div>
 
-                    <p><input class="w3-input" type="submit" value="Log In" form="userLogin"/> </p><br>
+                    <p><input class="w3-input" name="login" type="submit" value="Log In" form="userLogin"/> </p><br>
                 </form>
             </div>
         </div>
