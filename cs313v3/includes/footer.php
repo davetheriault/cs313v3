4de403fe6cb4d14c0682cde66163238d7f11547c
davault <davetheriault@gmail.com>
2016-02-06 21:01:26 -0700

    sffg

--- cs313v3/includes/footer.php
+++ cs313v3/includes/footer.php
@@ -1,21 +1,23 @@
     
 
-        </div>
-        <footer class="w3-container w3-teal w3-center">
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="about.php">About</a></li>
-                
-            </ul>
-        </footer>
-        <script>
-            var sh = $(window).height();
-            var dh = $(document).height();
-            if (dh < sh) {
-                var foot = document.getElementsByTagName("FOOTER");
-                foot.style.position = "fixed";
-                foot.style.bottom = "0";
-            }
-        </script>
-    </body>
+</div>
+<footer class="w3-container w3-teal w3-center">
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+
+    </ul>
+</footer>
+<script>
+    function fixedfooter() {
+        var sh = $(window).height();
+        var dh = $(document).height();
+        if (dh < sh) {
+            var foot = document.getElementsByTagName("FOOTER");
+            foot.style.position = "fixed";
+            foot.style.bottom = 0;
+        }
+    }
+</script>
+</body>
 </html>
