bb02c4abcdf6957dcc382ac847c4ce2559b3ff01
davault <davetheriault@gmail.com>
2016-02-06 19:59:35 -0700

    fgfgf

--- cs313v3/includes/management.php
+++ cs313v3/includes/management.php
@@ -1,5 +1,5 @@
 <?php $basename = basename(__FILE__); ?>
-<?php echo $basename; ?>
+
 <?php
     echo ''
        . '<div class="w3-col s4 m4 l4">'
@@ -10,25 +10,25 @@
        .     '<div class="w3-container w3-padding mngmnt">'
        .         '<ul class="w3-ul">'
        .             '<li';
-    if ($basename == 'collection.php') {
+    if ($basename == 'management.php collection.php') {
         echo ' class="w3-blue"';
     }
     echo '>'
        . '<a href="collection.php"><i class="fa fa-film"></i> My Collection</a></li>'
        . '<li';
-    if ($basename == 'addmovie.php') {
+    if ($basename == 'management.php addmovie.php') {
         echo ' class="w3-blue"';
     }
     echo '>'
        . '<a href="addmovie.php"><i class="fa fa-plus"></i> Add a Movie</a></li>'
        . '<li'; 
-    if ($basename == 'account.php') {
+    if ($basename == 'management.php account.php') {
         echo 'class="w3-blue"';
     }
     echo '>'
        . '<a href="account.php"><i class="fa fa-cog"></i> My Account</a></li>'
        . '<li'; 
-    if ($basename == 'find.php') {
+    if ($basename == 'management.php find.php') {
         echo ' class="w3-blue"';
     } 
     echo '>'
