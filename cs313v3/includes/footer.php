0949328d95e80e205458523062105b52dd6c934b
davault <davetheriault@gmail.com>
2016-02-06 21:06:13 -0700

    lcne

--- cs313v3/includes/footer.php
+++ cs313v3/includes/footer.php
@@ -1,7 +1,7 @@
     
 
 </div>
-<footer class="w3-container w3-teal w3-center">
+<footer class="w3-container w3-teal w3-center" onload="fixedfooter()">
     <ul>
         <li><a href="index.php">Home</a></li>
         <li><a href="about.php">About</a></li>
@@ -9,6 +9,7 @@
     </ul>
 </footer>
 <script>
+
     function fixedfooter() {
         var sh = $(window).height();
         var dh = $(document).height();
