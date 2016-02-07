868bbb16082a3c0dd6532900344df48560d37c8a
davault <davetheriault@gmail.com>
2016-02-06 17:01:45 -0700

    lohbnejd

--- cs313v3/includes/management.php
+++ cs313v3/includes/management.php
@@ -5,13 +5,13 @@
         </h3>
         <div class="w3-container w3-padding mngmnt">
             <ul class="w3-ul">
-                <li <?php if (basename(__FILE__) = 'collection.php'){echo 'class="w3-blue"';} ?> >
+                <li <?php if (basename(__FILE__) == 'collection.php'){echo 'class="w3-blue"';} ?> >
                     <a href="collection.php"><i class="fa fa-film"></i> My Collection</a></li>
-                <li <?php if (basename(__FILE__) = 'addmovie.php'){echo 'class="w3-blue"';} ?> >
+                <li <?php if (basename(__FILE__) == 'addmovie.php'){echo 'class="w3-blue"';} ?> >
                     <a href="addmovie.php"><i class="fa fa-plus"></i> Add a Movie</a></li>
-                <li <?php if (basename(__FILE__) = 'account.php'){echo 'class="w3-blue"';} ?> >
+                <li <?php if (basename(__FILE__) == 'account.php'){echo 'class="w3-blue"';} ?> >
                     <a href="account.php"><i class="fa fa-cog"></i> My Account</a></li>
-                <li <?php if (basename(__FILE__) = 'find.php'){echo 'class="w3-blue"';} ?> >
+                <li <?php if (basename(__FILE__) == 'find.php'){echo 'class="w3-blue"';} ?> >
                     <a href="find.php"><i class="fa fa-search"></i> Find Movies</a></li>
             </ul>
         </div>
