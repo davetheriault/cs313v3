ecde5e6ec55ffaeff8f8540b563e6b502d5dda7a
davault <davetheriault@gmail.com>
2016-02-09 21:10:29 -0700

    tra 6

--- cs313v3/tra6.php
+++ cs313v3/tra6.php
@@ -4,6 +4,19 @@
 
 <?php include 'includes/header.php'; ?>
 
+<?php 
+    if (isset($_POST['book'])){
+        $db->exec('INSERT INTO scripture (book, chapter, verse, content)'
+                . 'VALUES ("'.$_POST['book'].'", "'.$_POST['chapter'].'", "'.$_POST['verse'].'", "'.$_POST['content'].'")');
+    }
+    
+    if (isset($_POST['topic'])){
+        foreach ($_POST['topic'] as $top) {
+            $db->exec('INSERT INTO topics (name) VALUES ("'.$top.'")');
+        }
+    }
+?>
+
 <main class="w3-container">
     <div class="w3-row">
         <div class="rel w3-col s4 m4 l4">
@@ -13,21 +26,21 @@
                     
                     <form id="addScript" method="post" action="tra6.php">
                         <label>Book:</label>
-                        <input class="w3-input" type="text" name="book" />
+                        <input class="w3-input" type="text" name="book" required="required" />
                         
                         <label>Chapter:</label>
-                        <input type="number" class="w3-input" name="chapter" />
+                        <input type="number" class="w3-input" name="chapter" required="required"/>
                         
                         <label>Verse:</label>
-                        <input type="number" class="w3-input" name="verse" />
+                        <input type="number" class="w3-input" name="verse" required="required"/>
                         
                         <label>Content:</label>
-                        <textarea rows="9" cols="70" name="content" class="w3-input"></textarea>
+                        <textarea rows="9" cols="70" name="content" class="w3-input" required="required"></textarea>
                         
-                        <label>Topics:</label>
+                        <label>Topics:</label><br/>
                         <?php 
                         foreach ($db->query('SELECT name FROM topics ORDER BY name ASC') as $topic) {
-                            echo '<input type="checkbox" name="topic" value="'.$topic.'" />'.$topic.'<br/>';
+                            echo '<input type="checkbox" name="topic" value="'.$topic['name'].'" />'.$topic['name'].'<br/>';
                         }
                         ?>
                         
@@ -54,7 +67,12 @@
                         } else {
                             foreach ($db->query('SELECT book, chapter, verse, content FROM scripture') as $row) {
                                 echo '<li><strong>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</strong> <br/>"'
-                                . $row['content'] . '"</li>';
+                                . $row['content'] . '"'
+                                        . '<ul class="w3-ul">';
+                                foreach ($db->query('SELECT name FROM topics ORDER BY name ASC') as $tpc) {
+                                    echo '<li>'.$tpc['name'].'</li>';
+                                }
+                                echo '</ul>';
                             }
                         }
                         ?>
