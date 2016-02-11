4419d93702c86e57bd96df106882d9fb83536ad8
davault <davetheriault@gmail.com>
2016-02-10 21:42:02 -0700

    dlkj dd d ddd

--- cs313v3/tra6.php
+++ cs313v3/tra6.php
@@ -105,6 +105,18 @@ if (isset($_POST['topic'])) {
                     </form>
                 </div>
             </div>
+            <div class="w3-container w3-card-4 w3-white w3-padding-0 w3-margin">
+                <h2 class="w3-red w3-padding w3-margin-0">Search</h2>
+                <div class="w3-container w3-padding-bottom-16" style="padding-top: 15px; padding-bottom: 15px;">
+                    <p>Find entries by book...</p>
+                    <form id="ookbay" method="get" action="tra5.php">
+                        <label>Book:</label>
+                        <input class="w3-input" type="text" name="book" />
+                        
+                        <p><input class="w3-btn w3-blue" type="submit" value="Find" form="ookbay" /> </p>
+                    </form>
+                </div>
+            </div>
         </div>
         <div class="rel w3-col s8 m8 l8">
 
