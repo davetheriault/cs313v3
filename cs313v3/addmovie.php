918fe4388e1598d45917a6375739019934c35488
davault <davetheriault@gmail.com>
2016-02-17 21:33:10 -0700

    add movie

--- cs313v3/addmovie.php
+++ cs313v3/addmovie.php
@@ -12,29 +12,39 @@
                 <form id="addmovie" method="post" action="addmovieconfirm.php" class="w3-form" >
                     <ul class="w3-ul">
                         <li>
-                        <label for="title">Movie Title:</label>
-                        <input class="w3-input" name="title" id="title" type="text" />
+                            <label for="title">Movie Title:</label>
+                            <input class="w3-input" name="title" id="title" type="text" required />
                         </li>
                         <li>
-                        <label for="mpaa">Rating:</label>
-                        <select class="w3-select" name="mpaa" id="mpaa">
-                            <option disabled="disabled" selected>Select an option...</option>
-                            <option value="G">G <img src="images/G.jpg" /></option>
-                            <option value="PG">PG <img src="images/PG.jpg" /></option>
-                            <option value="PG-13">PG-13 <img src="images/PG-13.jpg" /></option>
-                            <option value="R">R <img src="images/R.jpg" /></option>
-                        </select>
+                            <label for="mpaa">Rating:</label>
+                            <select class="w3-select" name="mpaa" id="mpaa" required >
+                                <option disabled="disabled" selected>Select an option...</option>
+                                <option value="G">G <img src="images/G.jpg" /></option>
+                                <option value="PG">PG <img src="images/PG.jpg" /></option>
+                                <option value="PG-13">PG-13 <img src="images/PG-13.jpg" /></option>
+                                <option value="R">R <img src="images/R.jpg" /></option>
+                            </select>
                         </li>
                         <li>
-                        <label for="runtime">Runtime:</label>
-                        <input class="w3-input" type="time" name="runtime" id="runtime" />
+                            <label for="runtime">Runtime:</label>
+                            <input class="w3-input" type="time" name="runtime" id="runtime" />
                         </li>
                         <li>
-                        <label for="year">Release Year:</label>
-                        <input class="w3-input" type="text" name="year" id="year" />
+                            <label for="year">Release Year:</label>
+                            <input class="w3-input" type="text" name="year" id="year" required />
                         </li>
                         <li>
-                            <input class="w3-btn" type="submit" value="+Add Movie" form="addmovie" name="addsubmit"/>
+                            <select class="w3-select" name="genre[]" id="mpaa" required multiple >
+                                <option disabled="disabled" selected>Ctrl+click to select multiple genres...</option>
+                                <?php
+                                foreach ($db->query('SELECT name FROM genre') as $gnre) {
+                                    echo '<option value="' . $gnre . '">' . $gnre . '</option>';
+                                }
+                                ?>
+                            </select>
+                        </li>
+                        <li>
+                            <input class="w3-btn w3-padding w3-green" type="submit" value="+Add Movie" form="addmovie" name="addsubmit"/>
                         </li>
                     </ul>
                 </form>
--- cs313v3/addmovieconfirm.php
+++ cs313v3/addmovieconfirm.php
@@ -1,6 +1,35 @@
+<?php require 'includes/session.php'; ?>
+<?php require 'includes/dbConnect.php'; ?>
+
 <?php
 
-
+if (isset($_POST['addsubmit'])) {
+    if (!is_numeric($_POST['year']) || strlen($_POST['year']) != 4) {
+        $_SESSION['message'] = 'Release Year must be a four digit year';
+        redirect_to('addmovie.php');
+        exit;
+    }
+    if (!preg_match('/^(19)/', $_POST['year']) || !preg_match('/^(20)/', $_POST['year'])) {
+        $_SESSION['message'] = 'Invalid Release Year';
+        redirect_to('addmovie.php');
+        exit;
+    }
+    
+    $movtitle = $_POST['title'];
+    $mpaa = $_POST['mpaa'];
+    $run = $_POST['runtime'];
+    $year = $_POST['year'];
+    
+    $insert  = 'INSERT INTO movie (title, mpaa, runtime, release_year) ';
+    $insert .= 'VALUES ("'.$movtitle.'", "'.$mpaa.'", "'.$run.'", "'.$year.'") ';
+    
+    $addMov = $db->prepare($insert);
+    $addMov->execute();
+    
+    
+    
+    redirect_to('collection.php');
+}
 
 
 ?>
