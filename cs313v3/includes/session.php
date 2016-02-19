8fe5c404c78d4be501ac109c74d4f46366eeb2bd
davault <davetheriault@gmail.com>
2016-02-18 22:54:30 -0700

    add movie

--- cs313v3/includes/session.php
+++ cs313v3/includes/session.php
@@ -4,9 +4,9 @@ session_start();
 
 function message() {
     if (isset($_SESSION["message"])) {
-        $output = "<div class=\"message w3-card-4 w3-red\">";
+        $output = "<div style=\"width: 100%; position: relative; overflow: hidden;\"><div class=\"message w3-card-4 w3-red\">";
         $output .= htmlentities($_SESSION["message"]);
-        $output .= "</div><div class=\"msg\">&nbsp;</div>";
+        $output .= "</div><div class=\"msg\">&nbsp;</div></div>";
 
         // clear message after use
         $_SESSION["message"] = null;
