c53890b7811331aac105e9e8b96c653c8204d6ca
davault <davetheriault@gmail.com>
2016-02-19 14:30:03 -0700

    sljdaoienfif

--- cs313v3/collection.php
+++ cs313v3/collection.php
@@ -20,11 +20,11 @@
                         <?php
                         foreach (($db->query('SELECT movie_id FROM movie2user WHERE user_id = "' . $_SESSION['user_id'] . '"')) as $movie) {
                             foreach (($db->query('SELECT * FROM movie WHERE id = "' . $movie['movie_id'] . '" ORDER BY title ASC')) as $info) {
-                                echo '<li>'
+                                echo '<li><div class="w3-container">'
                                 . '<div class="w3-half"><strong><a href="movieinfo.php?title=' . htmlentities($info['title']) . '">' . $info['title'] . '</a></strong></div>'
                                 . '<div class="w3-quarter"><img src="../cs313v3/images/' . $info['mpaa'] . '.jpg" alt="' . $info['mpaa'] . '"/></div>'
                                 . '<div class="w3-quarter">(' . $info['release_year'] . ')</div>'
-                                . '</li>';
+                                . '</div></li>';
 
 
                                 /*  echo '<li><h4><a href="movieinfo.php?title=' . htmlentities($info['title']) . '">' . $info['title'] . '</a></h4>'
