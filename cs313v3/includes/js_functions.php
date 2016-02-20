69754218b8887cc42882a8c85f9155450e0bbc1e
davault <davetheriault@gmail.com>
2016-02-20 16:59:03 -0700

    ajax results

--- cs313v3/includes/js_functions.php
+++ cs313v3/includes/js_functions.php
@@ -9,7 +9,7 @@
             xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
         }
         xmlhttp.onreadystatechange = function () {
-             //   document.getElementById("results").innerHTML = xmlhttp.responseText;
+                document.getElementById("results").innerHTML = xmlhttp.responseText;
             
             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                 document.getElementById("results").innerHTML = xmlhttp.responseText;
