4a192f472c4e8c99a3debaa11d92d6e6e70a7a34
davault <davetheriault@gmail.com>
2016-02-20 16:56:44 -0700

    ajax results

--- cs313v3/includes/js_functions.php
+++ cs313v3/includes/js_functions.php
@@ -3,23 +3,19 @@
 
         if (window.XMLHttpRequest) {
             // code for IE7+, Firefox, Chrome, Opera, Safari
-            window.alert('window.XMLHttpRequest');
             xmlhttp = new XMLHttpRequest();
         } else {
             // code for IE6, IE5
             xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
         }
         xmlhttp.onreadystatechange = function () {
-            window.alert('onReadyStateChange called');
-                document.getElementById("results").innerHTML = xmlhttp.responseText;
+             //   document.getElementById("results").innerHTML = xmlhttp.responseText;
             
             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                window.alert('ready state 4 status 200');
                 document.getElementById("results").innerHTML = xmlhttp.responseText;
             }
         };
         if (add === undefined) {
-            window.alert('add undefined')
             xmlhttp.open("GET", "includes/results.php?find=" + str, true);
         } else {
             xmlhttp.open("GET", "includes/results.php?find=" + str + "&add=" + add, true);
@@ -33,7 +29,6 @@
         if (find == undefined) {
             find = '';
         }
-        window.alert(find);
         showResults(find, noadd);
     }
 
