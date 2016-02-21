4f5402d39ba1d7d70c536c638bc550dc34bcbf69
davault <davetheriault@gmail.com>
2016-02-20 23:23:29 -0700

    ajax

--- cs313v3/includes/addmovieajax.php
+++ cs313v3/includes/addmovieajax.php
@@ -1,6 +1,15 @@
 <?php
-    if (isset($_GET['user'])) {
-    $add = $db->prepare('INSERT INTO movie2user (user_id, movie_id) VALUES ("'.$_GET['user'].'", "'.$_GET['mov'].'")');
+
+$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+$dbName = "dave";
+
+$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+if (isset($_GET['user'])) {
+    $add = $db->prepare('INSERT INTO movie2user (user_id, movie_id) VALUES ("' . $_GET['user'] . '", "' . $_GET['mov'] . '")');
     $add->execute();
 }
 ?>
