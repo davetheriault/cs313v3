918fee8e565513cce47c986dea9b79326692c094
davault <davetheriault@gmail.com>
2016-02-20 18:43:27 -0700

    ajax results

--- cs313v3/includes/results.php
+++ cs313v3/includes/results.php
@@ -1,12 +1,19 @@
 
 
 <?php
-include 'includes/dbConnect.php';
+
+$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+$dbName = "dave";
+
+$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
 echo '<div class="w3-container w3-card-4 w3-white w3-padding-0 w3-margin">'
-   . '<h3 class="w3-red w3-padding w3-margin-0">Results for &quot;' . $_GET['find'] . '&quot;</h3>'
-   .      '<div class="w3-container w3-padding">'
-   .         '<ul class="w3-ul">';
+ . '<h3 class="w3-red w3-padding w3-margin-0">Results for &quot;' . $_GET['find'] . '&quot;</h3>'
+ . '<div class="w3-container w3-padding">'
+ . '<ul class="w3-ul">';
 
 foreach ($db->query('SELECT * FROM movie WHERE title LIKE "%' . htmlspecialchars($_GET['find']) . '%" ORDER BY title') as $info) {
     //$owns = check_ownership($_SESSION['user_id'], $info['id']);
@@ -24,5 +31,4 @@ foreach ($db->query('SELECT * FROM movie WHERE title LIKE "%' . htmlspecialchars
     echo '</li>';
 }
 echo '</ul></div></div>';
-
 ?>
