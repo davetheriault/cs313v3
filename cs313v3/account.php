d1cd00d17d09b6b02674e653d3d6dcb9134b8b92
davault <davetheriault@gmail.com>
2016-02-23 00:17:58 -0700

    account manage

--- cs313v3/account.php
+++ cs313v3/account.php
@@ -13,7 +13,8 @@
             <div class="w3-container w3-card-4 w3-white w3-padding-0 w3-margin">
                 <h3 class="w3-red w3-padding w3-margin-0"><span style="text-transform: uppercase;"><?php echo $_SESSION['username']; ?></span>&apos;s Movie Collection</h3>
                 <div id="collection" class="w3-container w3-padding" >
-                    <div><div class="w3-col s0 m1 l2">&nbsp;</div>
+                    <div class="w3-container">
+                        <div class="w3-col s0 m1 l2">&nbsp;</div>
                     <table class="w3-table w3-col s12 m10 l8">
                         <tr>
                             <td>
